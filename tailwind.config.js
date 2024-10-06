b54bb5ee15bbcbc053d53f3d48f0c3ebd53d5128
Vhs4 <vhszera44444@gmail.com>
2024-10-06 20:58:25 -0300

    FEAT: Nova fonte pra game

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,14 +2,15 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
-    backgroundImage: {
-      'level1': "linear-gradient(#63c4eb, #E0F6FF)",
+    extend: {
+      backgroundImage: {
+        'level1': "url(/static/images/europa.jpg)",
+      },
+      animation: {
+        'pipe-level1': 'pipe-animations 1.5s infinite linear',
+        'cloud-level1': 'cloud 20s infinite linear',
+      }
     },
-    animation: {
-      'pipe-level1': 'pipe-animations 1.5s infinite linear',
-      'cloud-level1': 'cloud 20s infinite linear',
-    }
   },
   plugins: [],
 }
